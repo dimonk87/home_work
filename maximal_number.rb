ac79c64a048b3193048d523d3deedcbeedf8569e
dimonk87 <dimon.k87@gmail.com>
2016-11-02 15:59:14 +0200

    Add files via upload

---
+++ maximal_number.rb
@@ -0,0 +1,4 @@
+string1 = "123 654 99999"
+array1 = string1.split(' ')
+
+puts array1.max
--- maximum_number_of_digits.rb
+++ maximum_number_of_digits.rb
@@ -1,5 +1,4 @@
 string = "123dfg654dg99999"
 array = string.split(/[a-z]/)
 
-
-puts array.max
+puts array.max.size
